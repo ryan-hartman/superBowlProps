5ad56cc185837e5b6d363aadfc0c56d8129480f7
Ryan Hartman <ryan.hartman.rh@gmail.com>
2021-02-06 23:29:31 -0500

    Cleanup/Show Picks

--- src/pages/SuperBowl/Admin/index.svelte
+++ src/pages/SuperBowl/Admin/index.svelte
@@ -2,70 +2,8 @@
 
     import { Field,Input,Button, Details, Modal, Radio } from 'svelte-chota'
     import VirtualList from '@sveltejs/svelte-virtual-list';
-
-    let props = [
-        {
-            bet: "Length of the National Anthem",
-            points: 3,
-            sideOne: "Over 203.5 Seconds",
-            sideTwo: "Under 203.5 Seconds"
-        },
-        {
-            bet: "Will the coin toss be heads or tails?",
-            points: 3,
-            sideOne: "Heads",
-            sideTwo: "Tails"
-        },
-        {
-            bet: "Which team will win the coin toss?",
-            points: 5,
-            sideOne: "Kansas City",
-            sideTwo: "Tampa Bay"
-        },
-        {
-            bet: "Which team will score first?",
-            points: 5,
-            sideOne: "Kansas City",
-            sideTwo: "Tampa Bay"
-        },
-        {
-            bet: "Jersey Number of the first player to score",
-            points: 5,
-            sideOne: "Odd",
-            sideTwo: "Even"
-        },
-        {
-            bet: "Which half will have more points scored?",
-            points: 5,
-            sideOne: "First Half",
-            sideTwo: "Second Half"
-        },
-        {
-            bet: "Will there be a successful fieldgoal over 49.5 yards?",
-            points: 7,
-            sideOne: "Yes",
-            sideTwo: "No"
-        },
-        {
-            bet: "Which team will score last?",
-            points: 7,
-            sideOne: "Kansas City",
-            sideTwo: "Tampa Bay"
-        },
-        {
-            bet: "Which team will win the game?",
-            points: 10,
-            sideOne: "Kansas City",
-            sideTwo: "Tampa Bay"
-        },
-        {
-            bet: "Total Points Scored in the Game",
-            points: 10,
-            sideOne: "Over 54.5",
-            sideTwo: "Under 54.5"
-        },
-        
-    ];
+    import {Props} from "../../../data.js"
+    let props = Props
 
     let propSubmission = {
         propPicks: {
--- src/pages/SuperBowl/index.svelte
+++ src/pages/SuperBowl/index.svelte
@@ -1,72 +1,11 @@
 <script>
     import { Field,Input,Button, Details, Modal, Radio } from 'svelte-chota'
     import VirtualList from '@sveltejs/svelte-virtual-list';
-
+    import {Props} from "../../data"
+    import {isActive, url} from '@roxi/routify'
     let modalOpen = false;
 
-    let props = [
-        {
-            bet: "Length of the National Anthem",
-            points: 3,
-            sideOne: "Over 203.5 Seconds",
-            sideTwo: "Under 203.5 Seconds"
-        },
-        {
-            bet: "Will the coin toss be heads or tails?",
-            points: 3,
-            sideOne: "Heads",
-            sideTwo: "Tails"
-        },
-        {
-            bet: "Which team will win the coin toss?",
-            points: 5,
-            sideOne: "Kansas City",
-            sideTwo: "Tampa Bay"
-        },
-        {
-            bet: "Which team will score first?",
-            points: 5,
-            sideOne: "Kansas City",
-            sideTwo: "Tampa Bay"
-        },
-        {
-            bet: "Jersey Number of the first player to score",
-            points: 5,
-            sideOne: "Odd",
-            sideTwo: "Even"
-        },
-        {
-            bet: "Which half will have more points scored?",
-            points: 5,
-            sideOne: "First Half",
-            sideTwo: "Second Half"
-        },
-        {
-            bet: "Will there be a successful fieldgoal over 49.5 yards?",
-            points: 7,
-            sideOne: "Yes",
-            sideTwo: "No"
-        },
-        {
-            bet: "Which team will score last?",
-            points: 7,
-            sideOne: "Kansas City",
-            sideTwo: "Tampa Bay"
-        },
-        {
-            bet: "Which team will win the game?",
-            points: 10,
-            sideOne: "Kansas City",
-            sideTwo: "Tampa Bay"
-        },
-        {
-            bet: "Total Points Scored in the Game",
-            points: 10,
-            sideOne: "Over 54.5",
-            sideTwo: "Under 54.5"
-        },
-        
-    ];
+    let props = Props
 
     let propSubmission = {
         propPicks: {
@@ -122,11 +61,19 @@
                 <tr>
                     <th class="thStyle">Name</th>
                     <th class="thStyle">Points</th>
+                    <th class="thStyle">Total Points</th>
+                    <th class="thStyle">Total Yards</th>
                 </tr>
                 {#each results as result}
                     <tr>
-                        <td class="tdStyle">{result.Name}</td>
+                        <td class="tdStyle">
+                            <a href={"/SuperBowl/"+$url(result.Name)}>
+                                {result.Name}
+                            </a>
+                        </td>
                         <td class="tdStyle">{result.points}</td>
+                        <td class="tdStyle">{result.tb1}</td>
+                        <td class="tdStyle">{result.tb2}</td>
                     </tr>
                 {/each}
             </table>
